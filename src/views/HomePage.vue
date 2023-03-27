c7d882c9c5436dbcc7f6600e231443313fbde281
Simone <sdiricco.91@gmail.com>
2023-03-27 21:46:25 +0200

    first commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,18 @@
+/* eslint-disable */
+
+<template>
+  <ion-page>
+    <ion-header>
+      <SearchToolbar />
+    </ion-header>
+
+    <ion-content :fullscreen="true"> </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts" setup>
+import SearchToolbar from "@/components/SearchToolbar.vue";
+
+/* IONIC COMPONENTS */
+import { IonContent, IonHeader, IonPage } from "@ionic/vue";
+</script>
